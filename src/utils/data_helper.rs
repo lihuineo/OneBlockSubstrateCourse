ea763854aa5adc53829ed91ea867d2a75195ea91
Li Hui <lihuineo@outlook.com>
2024-03-28 15:53:33 +0800

    bubble sort

--- src/utils/data_helper.rs
+++ src/utils/data_helper.rs
@@ -3,28 +3,28 @@ pub struct DataHelper {}
 #[allow(dead_code)]
 impl DataHelper {
     /*
-    基础要求：固定类型（比如i32）的数组排序
+    固定i32类型的数组冒泡排序
     */
     pub fn sort_basic(nums: &mut Vec<i32>) {
         assert!(!nums.is_empty());
-        for i in 0..nums.len() {
-            for j in i..nums.len() {
-                if nums[i] > nums[j] {
-                    nums.swap(i, j);
+        for i in 0..nums.len() - 1 {
+            for j in 0..nums.len() - i - 1 {
+                if nums[j] > nums[j + 1] {
+                    nums.swap(j, j + 1);
                 }
             }
         }
     }
 
     /*
-    提高部分：能够使用范型和PartialOrd实现对任意类型的排序
+    实现对任意类型的冒泡排序
      */
     pub fn sort_advanced<T: PartialOrd>(nums: &mut Vec<T>) {
         assert!(!nums.is_empty());
-        for i in 0..nums.len() {
-            for j in i..nums.len() {
-                if nums[i] > nums[j] {
-                    nums.swap(i, j);
+        for i in 0..nums.len() - 1 {
+            for j in i..nums.len() - i - 1 {
+                if nums[j] > nums[j + 1] {
+                    nums.swap(j, j + 1);
                 }
             }
         }
